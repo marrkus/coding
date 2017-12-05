b495b11857bee7d91549c148a281d48c0c7b136c
Markus Nissinen <markus.nissinen42@gmail.com>
2017-12-05 14:44:08 +0000

    Add files via upload

---
+++ manifests/init.pp
@@ -0,0 +1,77 @@
+class coding {
+
+        exec { 'apt-get update':
+		command => '/usr/bin/apt-get update',
+		refreshonly => true,
+                }
+
+        package { apache2:
+                require => Exec['apt-get update'],
+                ensure => 'installed',
+                allowcdrom => 'true',
+                }
+	
+	service { 'apache2':
+		ensure => 'true',
+		enable => true,
+		require => Package['apache2'],
+		}
+
+	file { '/home/xubuntu/public_html':
+		ensure => 'directory',     
+		}
+
+	file { '/home/xubuntu/public_html/index.html':
+		content => template('coding/index.html.erb'),
+		require => File['/home/xubuntu/public_html'],
+		}
+
+	file { '/etc/apache2/mods-enabled/userdir.load':
+		ensure => 'link',
+		target => '/etc/apache2/mods-available/userdir.load',
+		notify => Service["apache2"],
+		require => Package["apache2"],
+		}
+
+	file { '/etc/apache2/mods-enabled/userdir.conf':
+		ensure => 'link',
+		target => '/etc/apache2/mods-available/userdir.conf',
+		notify => Service["apache2"],
+		require => Package["apache2"],
+		}
+
+        package { openjdk-8-jre:
+                require => Exec['apt-get update'],
+                ensure => 'installed',
+                allowcdrom => 'true',
+                }
+
+        package { eclipse:
+                require => Exec['apt-get update'],
+                ensure => 'installed',
+                allowcdrom => 'true',
+                }
+
+	file { '/etc/skel/Main.java':
+		ensure => 'file',
+		content => template('coding/Main.java.erb'),
+		owner => 'root',
+		group => 'root',
+		mode => '0644',
+		}
+
+	file { '/etc/skel/index.html':
+                ensure => 'file',
+                content => template('coding/index.html.erb'),
+                owner => 'root',
+                group => 'root',
+                mode => '0644',
+                }
+
+        exec { 'OpenEclipse':
+                command => '/usr/bin/eclipse &',
+                require => Package['eclipse'],
+                }
+}	
+
+
