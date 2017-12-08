1fd13aae4006e13976f93fbe73f0d18d9e29d1d7
Markus Nissinen <markus.nissinen42@gmail.com>
2017-12-08 14:15:25 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,12 +5,11 @@ sudo apt-get -y install puppet git
 
 git clone https://github.com/marrkus/coding.git
 
-
 cd coding/modules
 sudo cp -r coding/ /etc/puppet/modules/
 cd /etc/puppet/
-
 sudo puppet apply --modulepath modules/ -e 'class {"coding":}'
+
 firefox https://www.tutorialspoint.com/eclipse/eclipse_create_java_project.htm
 firefox https://www.w3schools.com/html/html_elements.asp
 firefox localhost/~$USER
