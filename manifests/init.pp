1949b9e03d65c4b67a5cc2b3c0441dd635551fa8
Markus Nissinen <markus.nissinen42@gmail.com>
2017-12-11 21:56:09 +0200

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@ class coding {
 
         exec { 'apt-get update':
 		command => '/usr/bin/apt-get update',
-		refreshonly => true,
+		refreshonly => 'true',
                 }
 
         package { apache2:
