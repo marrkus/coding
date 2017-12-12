e77bd4901f70ecdc7d5de40620d16a1528d26d7b
Markus Nissinen <markus.nissinen42@gmail.com>
2017-12-12 15:03:15 +0000

    Update codingstart.sh

--- codingstart.sh
+++ codingstart.sh
@@ -12,7 +12,7 @@ sudo puppet apply --modulepath modules/ -e 'class {"coding":}'
 
 firefox -new-tab -url https://www.tutorialspoint.com/eclipse/eclipse_create_java_project.htm
 firefox -new-tab -url https://www.w3schools.com/html/html_elements.asp
-firefox -new-tab -url 172.28.171.196/~$USER
+firefox -new-tab -url localhost/~$USER
 nano /home/xubuntu/public_html/index.html
 
 
